8619b2b58d1ca44a564b8c55a483dd20d4084535
ganer <ganeralkidz@gmail.com>
2019-12-15 21:33:43 -0500

    Day 6 done, Day 7 to do

--- Day6.py
+++ Day6.py
@@ -1,20 +1,18 @@
+# This class represents an object. It has a name, a list of other objects representing its orbits,
+# an int representing how many orbits it has, as well as another object that it's the orbit of.
 class Obj:
     def __init__(self, name):
         self.name = name
-        self.orbits = 0
+        self.orbit_count = 0
         self.orbit_list = []
+        self.orbit_of = None
 
     def add_orbit(self, orbit):
         self.orbit_list.append(orbit)
-
-    def get_orbit_names(self):
-        orbit_names = []
-        for o in self.orbit_list:
-            orbit_names.append(o.name)
-        return orbit_names
+        self.orbit_count += 1
 
     def __str__(self):
-        return self.name + ',' + str(self.orbits)
+        return self.name + ',' + str(self.orbit_count)
 
     def print_orbits(self):
         for o in self.orbit_list:
@@ -22,26 +20,52 @@ class Obj:
 
 
 def count_orbits(o):
-    x = o.orbits
+    """
+    Function used to count how many direct and indirect orbits an object has
+
+
+    """
+    x = o.orbit_count
     for orbit in o.orbit_list:
         x += count_orbits(orbit)
     return x
 
 
-object_dict = {}
+# Retrieving the objects using a dictionary
+objects = {}
 f = open('inputs/day6.txt', 'r')
 for line in f:
     line = line.strip('\n').split(')')
-    obj1 = Obj(line[0])
-    obj2 = Obj(line[1])
-    if obj1.name not in object_dict.keys():
-        object_dict[obj1.name] = obj1
-        object_dict[obj1.name].add_orbit(obj2)
-        object_dict[obj1.name].orbits += 1
+    obj_name_1 = line[0]
+    obj_name_2 = line[1]
+    if obj_name_1 not in objects:
+        objects[obj_name_1] = Obj(obj_name_1)
+    if obj_name_2 not in objects:
+        objects[obj_name_2] = Obj(obj_name_2)
+    objects[obj_name_1].add_orbit(objects[obj_name_2])
+    objects[obj_name_2].orbit_of = objects[obj_name_1]
 
-    else:
-        if obj2.name not in object_dict[obj1.name].get_orbit_names():
-            object_dict[obj1.name].add_orbit(obj2)
-            object_dict[obj1.name].orbits += 1
+# Part 1: Counting all the direct and indirect connections for the whole graph of orbits
+counter = 0
+for obj in objects.values():
+    counter += count_orbits(obj)
+print(counter)
 
+# Part 2: Counting how many objects are between 'YOU' and 'SAN'
+you = objects['YOU']
+san = objects['SAN']
+you_object_names = []
+san_object_names = []
 
+while you.name != 'COM':
+    you = you.orbit_of
+    you_object_names.append(you.name)
+
+while san.name != 'COM':
+    san = san.orbit_of
+    san_object_names.append(san.name)
+
+for obj_name in you_object_names:
+    if obj_name in san_object_names:
+        print(you_object_names.index(obj_name) + san_object_names.index(obj_name))
+        break
